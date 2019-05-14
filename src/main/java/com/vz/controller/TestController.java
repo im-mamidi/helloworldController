6ff2b47813ed4b77a1f2986f02231d945e916c28
im-mamidi <srinukv33@gmail.com>
2019-05-14 21:08:17 +0530

    added some files

--- src/main/java/com/vz/controller/TestController.java
+++ src/main/java/com/vz/controller/TestController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+	//private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping("/test")
 	public String printData() {
-		
 		return "Hello World";
 
 	}
